fc03d28dfd7b718230a29914c36e3ee7a8c6beb6
Aiden Benner <aiden.benner@gmail.com>
2016-01-08 19:55:08 +0000

    Trainer.java edited online with Bitbucket

--- src/Trainer.java
+++ src/Trainer.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 
 public class Trainer {
 	public static void main(String args[]) throws Exception{
-		NeuralNetwork n = new NeuralNetwork(3,2,2,1);
+		NeuralNetwork n = new NeuralNetwork(3,5,2,1);
 		
 //		JFrame jf = new JFrame();
 //		jf.setSize(500, 500);
@@ -11,20 +11,29 @@ public class Trainer {
 //		jf.add(n);
 //		jf.setVisible(true);
 //		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		float[] inputs = new float[]{0,0};
-		float[] target = new float[]{1};
+		double[] inputs = new double[]{0,0};
+		double[] target = new double[]{1};
 		
-		float[] inputs2 = new float[]{1,0};
-		float[] target2 = new float[]{1};
+		double[] inputs2 = new double[]{1,0};
+		double[] target2 = new double[]{1};
 		
-		float[] inputs3 = new float[]{1,1};
-		float[] target3 = new float[]{0};
+		double[] inputs3 = new double[]{1,1};
+		double[] target3 = new double[]{0};
 
-		for(int i = 0; i<10000; i++){
+		for(int i = 0; i<900000; i++){
 
 
 			n.train(inputs, target);
 			n.train(inputs3, target3);
+			
+			if(i % 10000 == 0){
+				System.out.println("Epoch " + i + "------------");
+				System.out.println(n.getResult(inputs)[0]);
+				System.out.println(n.getResult(inputs2)[0]);
+				System.out.println(n.getResult(inputs3)[0]);
+			}
+			
+			
 		}
 		
 		//TESTING WITH XOR
